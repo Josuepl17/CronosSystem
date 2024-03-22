94eab54d3ba6e74a980289aec1d77f588b704d21
Josuepl17 <136339183+Josuepl17@users.noreply.github.com>
2024-03-22 11:56:00 -0300

    top

---
+++ app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    
+    private $product;
+
+    public function __construct(Product $product){
+    
+        $this->product = $product;
+    
+    }
+
+
+public function index(){
+    $products = Product::all();
+    
+    return response()->json($products) ;
+}
+
+public function show($id){
+    $products = $this->product->find($id);
+    dd($products);
+
+    return response()->json($products) ;
+}
+
+public function save(Request $request){
+    $data = $request->all();
+     $this->product->create($data);
+
+    
+}
+
+public function update(Request $request){
+    $data = $request->all(); // pega todos os dados da requisição 
+    $product = $this->product->find($data['id']); // procura no banco pelos dado do ID recebido na variavel $data
+    $product->update($data); // atualiza a model com os dados recebidos
+    return response()->json($product); // retorna o item atualizado em json
+}
+
+public function delee($id){
+    $product = $this->product->find($id);
+    $product->delete();
+    return response()->json($product) ;
+}
+
+public function delete(Request $request){
+    
+    $data =  $this->product::destroy($request->id);
+    return response()->json($data) ;
+}
+
+
+}
---
+++ routes/api.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Http\Controllers\Api\ProductController;
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
+Route::get('/teste', function(){
+    echo "ola mundo";
+});
+
+Route::get('/products', [ProductController::class, 'index']); // busca Todos
+
+Route::post('/products', [ProductController::class, 'save']); // cria produto
+
+Route::get('/{id}', [ProductController::class, 'show']); // busca pelo id apenas 1
+
+Route::put('/products' , [ProductController::class, 'update']); // leva tanto pela url tantto pelo post  
+
+Route::delete('/{id}' , [ProductController::class, 'delete']); // leva tanto pela url tantto pelo post 
