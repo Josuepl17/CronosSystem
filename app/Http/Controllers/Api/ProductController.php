df91bf2587a5ba9052f589180b6e3139dca75e26
Josuepl17 <136339183+Josuepl17@users.noreply.github.com>
2024-04-16 23:52:00 -0300

    Aula 23

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -24,11 +24,23 @@ class ProductController extends Controller
 public function index(Request $request){
 
     $product = $this->product;
+
+        if ($request->has('coditions')){
+            $expressions = explode(';', $request->get('coditions'));
+        
+            foreach ($expressions as $e){
+                $exp = explode('=', $e);
+                $product = $product->where($exp[0], $exp[1]);
+            }
+        
+        }
     
         if($request->has('fields')){
         $fields = $request->get('fields');
-        dd($fields);
-        $product = $product->selectRaw($fields);
+        $fields = explode(',', $fields);
+       
+        $product = $product->select($fields)->paginate(2); // metodo paginete substituiu o metodo get 
+   
     }
   
  
